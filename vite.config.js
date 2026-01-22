28ae809d6a132fe267ac5eb46957e14ed309e9fc
Krushil-2126 <24dit043@charusat.edu.in>
2026-01-22 22:42:19 +0530

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Software-Grp-Project/',
+  base: '/software/',
   plugins: [react()],
 })
